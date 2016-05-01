fb06a791867cb0906a197d23a11040ece8e26150
Bhavesh Munot <bhavesh.munot1@gmail.com>
2016-05-01 20:04:44 +0530

    Fixed the bug, moved input outside the loop

--- CricNotify.py
+++ CricNotify.py
@@ -6,12 +6,13 @@ import pprint
 def Notify(title, message):
 	os.system('notify-send ' + '"' + str(title) + '"' + ' ' + '"' + str('\n' + message) + '"')
 
-while True:
-	res = requests.get("http://cricscore-api.appspot.com/csa")
-	res = res.json()
-	pp = pprint.PrettyPrinter(indent = 2)
-	pp.pprint(res)
-	match_id = input("Enter match ID: ")
+res = requests.get("http://cricscore-api.appspot.com/csa")
+res = res.json()
+pp = pprint.PrettyPrinter(indent = 2)
+pp.pprint(res)
+match_id = input("Enter match ID: ")
+
+while True:	
 	res = requests.get("http://cricscore-api.appspot.com/csa?id=" + str(match_id))
 	res = res.json()
 	Notify(res[0]['si'], res[0]['de'])
