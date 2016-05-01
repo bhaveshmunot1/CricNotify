6b8b03576eef37c84d7c5145e236d3fa15bd5e18
Bhavesh Munot <bhavesh.munot1@gmail.com>
2016-05-01 19:53:08 +0530

    Working code is done, need better UI

--- CricNotify.py
+++ CricNotify.py
@@ -1,6 +1,18 @@
 import os
+import requests
+from time import sleep
+import pprint
 
 def Notify(title, message):
-	os.system('notify-send ' + '"' + title + '"' + ' ' + '"' + message + '"')
+	os.system('notify-send ' + '"' + str(title) + '"' + ' ' + '"' + str('\n' + message) + '"')
 
-Notify('Title', 'This is a message!')
+while True:
+	res = requests.get("http://cricscore-api.appspot.com/csa")
+	res = res.json()
+	pp = pprint.PrettyPrinter(indent = 2)
+	pp.pprint(res)
+	match_id = input("Enter match ID: ")
+	res = requests.get("http://cricscore-api.appspot.com/csa?id=" + str(match_id))
+	res = res.json()
+	Notify(res[0]['si'], res[0]['de'])
+	sleep(30)
